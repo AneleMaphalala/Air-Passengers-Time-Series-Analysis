b164147dff37494c7792220aa47cbbf74806bbe4
Anele Maphalala <maphalalaanele@gmail.com>
2024-10-15 14:31:30 +0200

    docs: visualization

---
+++ Time Series Forecasting.R
@@ -0,0 +1,14 @@
+
+AirPassengers
+
+str(AirPassengers)
+class(AirPassengers)
+# Start Date 
+start(AirPassengers)
+# End Date
+end(AirPassengers)
+# How frequent data was recorded
+frequency(AirPassengers)
+cycle(AirPassengers)
+
+# Data Visualization
